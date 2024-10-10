94db0685791c33961cf2ff5ca52974e2934ee1c5
DavidMadLife <quansongngu13@gmail.com>
2024-10-10 13:37:26 +0700

    fix OTP

--- AVR.Application/ViewModels/Request/Auth/ResetPasswordRequest.cs
+++ AVR.Application/ViewModels/Request/Auth/ResetPasswordRequest.cs
@@ -9,7 +9,7 @@ namespace AVR.Application.ViewModels.Request.Auth
     public class ResetPasswordRequest
     {
         public string Email { get; set; }
-        public string Token { get; set; }
+        public string Otp { get; set; }
         public string NewPassword { get; set; }
         public string ConfirmPassword { get; set; }
     }
