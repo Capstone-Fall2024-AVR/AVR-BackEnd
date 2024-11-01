c87bc7e8d3e255fb045e087ea0275fc349d457fe
DavidMadLife <165918703+DavidMadLife@users.noreply.github.com>
2024-11-01 19:25:53 +0700

    Update ProjectProviderController.cs

--- AVR.WebAPI/Controllers/ProjectProviderController.cs
+++ AVR.WebAPI/Controllers/ProjectProviderController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AVR.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/projectproviders")]
     [ApiController]
     public class ProjectProviderController : BaseController
     {
