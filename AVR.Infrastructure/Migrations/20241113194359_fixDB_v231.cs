32bf83691aede62a817c18e8cbf74e2554be990b
DavidMadLife <165918703+DavidMadLife@users.noreply.github.com>
2024-11-14 08:12:17 +0700

    Update and rename 20241113194359_fixDB_v24.cs to 20241113194359_fixDB_v231.cs

--- AVR.Infrastructure/Migrations/20241113194359_fixDB_v231.cs
+++ AVR.Infrastructure/Migrations/20241113194359_fixDB_v231.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace AVR.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class fixDB_v24 : Migration
+    public partial class fixDB_v231 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
