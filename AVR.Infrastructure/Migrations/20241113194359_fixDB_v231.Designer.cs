4faef4d67ef94a96801353184d997a9f1f19fe56
DavidMadLife <165918703+DavidMadLife@users.noreply.github.com>
2024-11-14 08:27:55 +0700

    Update 20241113194359_fixDB_v231.Designer.cs

--- AVR.Infrastructure/Migrations/20241113194359_fixDB_v231.Designer.cs
+++ AVR.Infrastructure/Migrations/20241113194359_fixDB_v231.Designer.cs
@@ -13,7 +13,7 @@ namespace AVR.Infrastructure.Migrations
 {
     [DbContext(typeof(MyDbContext))]
     [Migration("20241113194359_fixDB_v231")]
-    partial class fixDB_v23
+    partial class fixDB_v231
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
