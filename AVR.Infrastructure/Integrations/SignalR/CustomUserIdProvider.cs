69d4de9d342eef44bc1bfd26cd41eeacd378b7dc
DavidMadLife <quansongngu13@gmail.com>
2024-11-24 09:34:31 +0700

    fix signalR

---
+++ AVR.Infrastructure/Integrations/SignalR/CustomUserIdProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVR.Infrastructure.Integrations.SignalR
+{
+    public class CustomUserIdProvider : IUserIdProvider
+    {
+        public string GetUserId(HubConnectionContext connection)
+        {
+            var claims = connection.User?.Claims.Select(c => $"{c.Type}: {c.Value}").ToList();
+            Console.WriteLine($"Claims: {string.Join(", ", claims)}");
+
+            var userId = connection.User?.FindFirst("id")?.Value;
+
+            Console.WriteLine($"CustomUserIdProvider: Retrieved UserId = {userId}");
+
+            return userId;
+        }
+
+    }
+}
