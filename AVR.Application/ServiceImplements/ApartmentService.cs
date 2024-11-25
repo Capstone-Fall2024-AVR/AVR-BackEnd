ab3eb4166ce00506e2b214e7753d38e4554eb0e4
luongpd-dev <135132004+luongpd-dev@users.noreply.github.com>
2024-11-25 13:14:40 +0700

    Update ApartmentService.cs

--- AVR.Application/ServiceImplements/ApartmentService.cs
+++ AVR.Application/ServiceImplements/ApartmentService.cs
@@ -314,6 +314,26 @@ namespace AVR.Application.ServiceImplements
             }
             // Ánh xạ kết quả trả về thành response
             var response = _mapper.Map<CreateApartmentResponse>(apartment);
+            
+            //find deposit value from Project Financial Contract
+            var projectfee = _unitOfWork.ProjectFinancialContractRepository
+                .Get(pf => pf.ProjectApartmentID == apartment.ProjectApartmentID &&
+                    pf.LowestPrice <= apartment.Price &&
+                    pf.HighestPrice > apartment.Price
+                ).FirstOrDefault();
+            if(projectfee != null )
+            {
+                response.DepositAmount = projectfee.DepositAmount;
+            }
+            
+            //find deposit value from Property Verification
+            var property = _unitOfWork.PropertyVerificationRepository
+                .Get(pr => pr.ApartmentOwnerApartmentID == apartment.ApartmentID
+                ).FirstOrDefault();
+            if (property != null )
+            {
+                response.DepositAmount = property.DepositValue;
+            }
             response.ProjectApartmentName = projectApartment.ProjectApartmentName; // Thêm tên dự án
             response.Images = apartmentImages.Select(img => new ApartmentImageResponse
             {
