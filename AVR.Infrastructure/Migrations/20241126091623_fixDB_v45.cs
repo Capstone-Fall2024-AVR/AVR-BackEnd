c9e7a82a996250c5ae66b573a09d9b7ecfc36555
DavidMadLife <165918703+DavidMadLife@users.noreply.github.com>
2024-11-26 21:30:40 +0700

    Update 20241126091623_fixDB_v45.cs

--- AVR.Infrastructure/Migrations/20241126091623_fixDB_v45.cs
+++ AVR.Infrastructure/Migrations/20241126091623_fixDB_v45.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace AVR.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class fixDB_v45 : Migration
+    public partial class fixDB_v46 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
