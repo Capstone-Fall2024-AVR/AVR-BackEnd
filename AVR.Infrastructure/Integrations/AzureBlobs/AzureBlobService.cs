191d3a72e38fc81dd0496108f1ae5c72e0a73110
DavidMadLife <165918703+DavidMadLife@users.noreply.github.com>
2024-11-29 18:21:40 +0700

    Update AzureBlobService.cs

--- AVR.Infrastructure/Integrations/AzureBlobs/AzureBlobService.cs
+++ AVR.Infrastructure/Integrations/AzureBlobs/AzureBlobService.cs
@@ -1,6 +1,7 @@
-﻿using AVR.Domain.Interfaces;
+using AVR.Domain.Interfaces;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
 using Microsoft.Extensions.Configuration;
 using SharpCompress.Archives;
 using SharpCompress.Archives.Rar;
@@ -30,36 +31,36 @@ namespace AVR.Infrastructure.Integrations.AzureBlobs
         public async Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType)
         {
             var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
-        
+
             // Tạo container nếu chưa tồn tại
             await containerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
-        
+
             var blobClient = containerClient.GetBlockBlobClient(fileName);
-        
+
             // Kích thước của mỗi block (4 MB trong ví dụ này)
             const int blockSize = 4 * 1024 * 1024;
             var blockList = new List<string>();
             byte[] buffer = new byte[blockSize];
             int bytesRead;
             int blockNumber = 0;
-        
+
             while ((bytesRead = await fileStream.ReadAsync(buffer, 0, blockSize)) > 0)
             {
                 var blockId = Convert.ToBase64String(Encoding.UTF8.GetBytes($"block-{blockNumber:D5}"));
                 using var memoryStream = new MemoryStream(buffer, 0, bytesRead);
-        
+
                 // Upload block lên Blob Storage
                 await blobClient.StageBlockAsync(blockId, memoryStream);
                 blockList.Add(blockId);
                 blockNumber++;
             }
-        
+
             // Commit các block đã upload
             await blobClient.CommitBlockListAsync(blockList, new BlobHttpHeaders
             {
                 ContentType = contentType
             });
-        
+
             // Trả về URL của file đã upload
             return blobClient.Uri.ToString();
         }
