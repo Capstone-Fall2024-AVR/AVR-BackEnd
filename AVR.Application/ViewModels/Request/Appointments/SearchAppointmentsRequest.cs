34da6ccdf48b33361c544d4e2dd982666dddf999
DavidMadLife <quansongngu13@gmail.com>
2024-12-06 17:12:05 +0700

    fix appointment

--- AVR.Application/ViewModels/Request/Appointments/SearchAppointmentsRequest.cs
+++ AVR.Application/ViewModels/Request/Appointments/SearchAppointmentsRequest.cs
@@ -15,6 +15,7 @@ namespace AVR.Application.ViewModels.Request.Appointments
         public DateTimeOffset? StartDate { get; set; }
         public DateTimeOffset? EndDate { get; set; }
         public string? Title { get; set; }
+        public Guid? TeamID { get; set; }
         public int PageIndex { get; set; } = 1;
         public int PageSize { get; set; } = 10;
     }
