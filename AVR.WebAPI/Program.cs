0021cae87952b155c8e170a6c68c0c1cca128cb2
DavidMadLife <165918703+DavidMadLife@users.noreply.github.com>
2024-12-07 23:42:52 +0700

    Update Program.cs

--- AVR.WebAPI/Program.cs
+++ AVR.WebAPI/Program.cs
@@ -141,7 +141,7 @@ app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapHub<NotificationHub>("/notificationHub");
-    endpoints.MapHub<ChatHub>("/chatHub");
+    endpoints.MapHub<NotificationHub>("/chatHub");
     endpoints.MapControllers();
 });
 
