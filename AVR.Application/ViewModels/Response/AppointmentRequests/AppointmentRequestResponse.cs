8136d7dd24af5e2d28dc78256b075ba764243555
luongpd-dev <135132004+luongpd-dev@users.noreply.github.com>
2024-12-17 23:59:31 +0700

    delete ReferenceCode in AppointmentRequest

--- AVR.Application/ViewModels/Response/AppointmentRequests/AppointmentRequestResponse.cs
+++ AVR.Application/ViewModels/Response/AppointmentRequests/AppointmentRequestResponse.cs
@@ -27,7 +27,6 @@ namespace AVR.Application.ViewModels.Response.AppointmentRequests
         public DateTimeOffset UpdateDate { get; set; }
         public string Username { get; set; } // Tên khách hàng
         public string PhoneNumber { get; set; } // Số điện thoại khách hàng
-        public string ReferenceCode { get; set; }
 
     }
 }
