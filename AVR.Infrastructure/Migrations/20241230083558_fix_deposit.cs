4cd0db0e4c5cdb09fd7de4dce80faddab68ccda4
luongpd-dev <135132004+luongpd-dev@users.noreply.github.com>
2024-12-30 15:39:03 +0700

    fix-deposit

---
+++ AVR.Infrastructure/Migrations/20241230083558_fix_deposit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AVR.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_deposit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ExpiryDate",
+                table: "ProjectFiles",
+                type: "datetimeoffset",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "DepositProfile",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "DepositProfile");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ExpiryDate",
+                table: "ProjectFiles",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldNullable: true);
+        }
+    }
+}
